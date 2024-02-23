00177d2ec0cb83a5ed1b0045f61b099ae593d455
Matthew Morikan <matthew@Matthew-Morikans-MacBook-Pro-2.local>
2024-02-23 18:44:00 -0500

    Join room and create room form

--- frontend/src/components/Forms/CreateRoomForm/index.jsx
+++ frontend/src/components/Forms/CreateRoomForm/index.jsx
@@ -1,5 +1,34 @@
 const CreateRoomForm = () => {
-    return <h1>Create Room Form</h1>;
+    return (
+        <form className="form col-md-12 mt-5">
+            <div className="form-group">
+                <input
+                    type="text"
+                    className="form-control my-2"
+                    placeholder="Enter your name"
+                />
+            </div>
+            <div className="form-group border bg-gray">
+                <div className="input-group d-flex align-items-center justify-content-center">
+                    <input
+                        type="text"
+                        className="form-control my-2 border-0"
+                        disabled
+                        placeholder="Room Code"
+                    />
+                    <div className="input-group-append">
+                        <button className="btn btn-primary btn-sm me-1" type="button">
+                            Generate
+                        </button>
+                        <button className="btn btn-outline-danger btn-sm me-2" type="button">
+                            Copy
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <button type="submit" className="mt-4 btn-primary btn-block form-control"> Generate Room </button>
+        </form>
+    );
 };
 
 export default CreateRoomForm;
--- frontend/src/components/Forms/index.jsx
+++ frontend/src/components/Forms/index.jsx
@@ -7,11 +7,11 @@ import "./index.css";
 const Forms = () => {
     return (
         <div className="row h-100 pt-5">
-            <div className="col-md-4 mt-5 form-box p-3 border border-primary rounded-2 mx-auto d-flex flex-column align-items-center">
+            <div className="col-md-4 mt-5 form-box py-3 px-5 border border-primary rounded-2 mx-auto d-flex flex-column align-items-center">
                 <h1 className= "text-primary fw-bold">Create Room</h1>
                 <CreateRoomForm />
             </div>
-            <div className="col-md-4 mt-5 form-box p-3 border border-primary rounded-2 mx-auto d-flex flex-column align-items-center">
+            <div className="col-md-4 mt-5 form-box py-3 px-5 border border-primary rounded-2 mx-auto d-flex flex-column align-items-center">
                 <h1 className= "text-primary fw-bold">Join Room</h1>
                 <JoinRoomForm />
             </div>
